29f2af9bd50a23e19b476e21a9752faadbb0ff8f
deepika-vuppugandla <deepikareddy61525@gmail.com>
2020-09-29 22:28:56 +0530

    Adding presentation here

---
+++ presentation1/src/presentation1/Interview.java
@@ -0,0 +1,15 @@
+package presentation1;
+
+public class Interview {
+
+	static String subject="java";
+	int rating=1;
+	static int Batchcode=101;
+	String name="deepika";
+	public Interview() {
+		
+	}
+	public void Answer(int a) {
+		System.out.println("a="+a);
+	}
+}
---
+++ presentation1/src/presentation1/Interview2.java
@@ -0,0 +1,16 @@
+package presentation1;
+
+public class Interview2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		System.out.println("subject="+Interview.subject);
+		System.out.println("BatchCode="+Interview.Batchcode);
+		Interview i1=new Interview();
+		System.out.println("rating="+i1.rating);
+		System.out.println("name="+i1.name);
+	    i1.Answer(5);
+	}
+
+}
